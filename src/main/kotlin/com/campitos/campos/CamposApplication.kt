dae61bc46633cbff7d17d071cb68879d64a0db31
daton <rapidclimate@gmail.com>
2021-02-19 14:46:02 -0600

    Initial commit

---
+++ src/main/kotlin/com/campitos/campos/CamposApplication.kt
@@ -0,0 +1,11 @@
+package com.campitos.campos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CamposApplication
+
+fun main(args: Array<String>) {
+	runApplication<CamposApplication>(*args)
+}
---
+++ src/test/kotlin/com/campitos/campos/CamposApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.campitos.campos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CamposApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
